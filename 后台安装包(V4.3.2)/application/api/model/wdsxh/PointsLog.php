8742d7ef55202039788a14acc7e4ef1acd5f6dda
JustWorking <3546124151@qq.com>
2025-08-29 14:15:59 +0800

    完成了

---
+++ 后台安装包(V4.3.2)/application/api/model/wdsxh/PointsLog.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace app\api\model\wdsxh;
+
+use think\Model;
+
+
+class PointsLog extends Model
+{
+
+    
+
+    
+
+    // 表名
+    protected $name = 'wdsxh_user_wechat_points_log';
+    
+    // 自动写入时间戳字段
+    protected $autoWriteTimestamp = 'integer';
+
+    // 定义时间戳字段名
+    protected $createTime = 'createtime';
+    protected $updateTime = false;
+    protected $deleteTime = false;
+
+    // 追加属性
+    protected $append = [
+        'change_text',
+    ];
+    
+
+    
+    public function getChangeList()
+    {
+        return ['1' => __('Change 1'), '2' => __('Change 2')];
+    }
+
+  
+    public function getChangeTextAttr($value, $data)
+    {
+        $value = $value ? $value : (isset($data['change']) ? $data['change'] : '');
+        $list = $this->getChangeList();
+        return isset($list[$value]) ? $list[$value] : '';
+    }
+
+
+      public function getSourceTextAttr($value, $data)
+    {
+        $value = $value ? $value : (isset($data['source']) ? $data['source'] : '');
+        $list = $this->getSourceList();
+        return isset($list[$value]) ? $list[$value] : '';
+    }
+     
+        public function getSourceList()
+    {
+        return ['1' => __('Source 1')];
+    }
+
+
+}
